53332d4813a46da62b68bc7c93dd8be4cfebe660
Amitesh Anand <aamitesh7@gmail.com>
2022-08-19 17:15:19 +0530

    chnage isPrime function to cover input  of 1

--- isPrime.js
+++ isPrime.js
@@ -1,8 +1,8 @@
 console.log("Find isPrime");
 function isPrime(n) {
   let factors = 0;
-  if (n < 1) {
-    console.log("Please enter valid positive number");
+  if (n < 2) {
+    console.log("Please enter valid positive number greater than 2");
   } else {
     for (let i = 2; i < Math.sqrt(n); i++) {
       if (n % i == 0) {
